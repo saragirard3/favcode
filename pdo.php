6a6516c28c8e95461cb3309a71e043bbaa9301ba
Sara <82391992+saragirard3@users.noreply.github.com>
2021-06-10 12:12:25 -0400

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 //PDO
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=girasgpm_portfolio','girasgpm_profile', 'ZcG9iF]_29W2');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=test','test', 'test');
 
 // See the 'errors' folder for details
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
